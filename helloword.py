7d3c46ffb1f03f6c5bffb6899077e8d1e977a8e1
MDrealcria <151069604+MDrealcria@users.noreply.github.com>
2023-11-30 15:17:26 -0300

    Create helloword.py
    first commit

---
+++ helloword.py
@@ -0,0 +1,7 @@
+print("helloword !")   # mostra na tela txt hello word!
+mensagem ="ola, tudo bem?"
+print(mensagem) #mostra a mensagem na tela
+print(mensagem[1]) #mostra a segunda letra na tela
+print(mensagem[0:4]) #mostra da primeira ate a quarta letra na tela
+print(mensagem[-1]) #mostra a ultima letra da mensagem
+print(mensagem[-4::])
